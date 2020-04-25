286d9b093d186064cacc367dacab1dc94161ca79
Dan Roberts <dan@adadevelopersacademy.org>
2020-04-25 12:55:12 -0700

    Tests and implementation for BST lookup and insert

--- src/data_structures/binary_search_tree.js
+++ src/data_structures/binary_search_tree.js
@@ -1,5 +1,81 @@
-class BinarySearchTree {
+class BSTNode {
+  constructor({ key, value, parent, left, right }) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = left;
+    this.right = right;
+  }
+}
 
+class BinarySearchTree {
+  constructor(Node = BSTNode) {
+    this.Node = Node;
+    this._count = 0;
+    this._root = undefined;
+  }
+
+  _findNode(key) {
+    // Retuns {node, parent}, either of which may be undefined
+    // Node undefined means the key isn't in the tree
+    // Parent undefined means node is the root
+    let parent = undefined;
+    let node = this._root;
+
+    while (node) {
+      if (key < node.key) {
+        parent = node;
+        node = node.left;
+      } else if (key > node.key) {
+        parent = node;
+        node = node.right;
+      } else { // equal
+        break;
+      }
+    }
+    return { node, parent }
+  }
+
+  insert(key, value = true) {
+    const { node, parent } = this._findNode(key);
+    if (node) {
+      // replace
+      node.value = value;
+
+    } else {
+      // new node
+      const newNode = new this.Node({ key, value, parent });
+      this._count += 1;
+
+      if (parent) {
+        if (key < parent.key) {
+          parent.left = newNode;
+        } else {
+          parent.right = newNode;
+        }
+      } else {
+        this._root = newNode;
+      }
+    }
+  }
+
+  lookup(key) {
+    // Hooray for optional chaining!
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+    return this._findNode(key).node?.value;
+  }
+
+  delete(key) {
+
+  }
+
+  count() {
+    return this._count;
+  }
+
+  forEach(callback) {
+
+  }
 }
 
 export default BinarySearchTree;
--- src/data_structures/binary_search_tree.test.js
+++ src/data_structures/binary_search_tree.test.js
@@ -6,7 +6,7 @@ import RedBlackTree from './red_black_tree';
 
 const dataStructures = [
   BinarySearchTree,
-  RedBlackTree,
+  // RedBlackTree,
 ];
 
 dataStructures.forEach(TargetDS => {
@@ -18,6 +18,92 @@ dataStructures.forEach(TargetDS => {
 
     it('starts empty', () => {
       expect(bst.count()).toBe(0);
+    });
+
+    describe('lookup', () => {
+      it('returns undefined on an empty tree', () => {
+        expect(bst.lookup('test')).toBe(undefined);
+      });
+
+      it('returns undefined if the key is not in the tree', () => {
+        const keys = ['many', 'keys', 'for', 'this', 'tree'];
+        keys.forEach((key, i) => {
+          bst.insert(key);
+        });
+
+        expect(bst.lookup('dne')).toBe(undefined);
+      });
+
+      it('finds the only record', () => {
+        bst.insert('test');
+        expect(bst.lookup('test')).toBeTruthy();
+      });
+
+      it('finds any extant record', () => {
+        const keys = ['many', 'keys', 'for', 'this', 'tree'];
+        keys.forEach(key => {
+          bst.insert(key);
+        });
+
+        keys.forEach(key => {
+          expect(bst.lookup(key)).toBeTruthy();
+        });
+
+        keys.reverse().forEach(key => {
+          expect(bst.lookup(key)).toBeTruthy();
+        });
+      });
+
+      it('returns the value associated with a record', () => {
+        const records = [
+          {key: 'one', value: 'first'},
+          {key: 'two', value: 'second'},
+          {key: 'three', value: 'third'},
+          {key: 'four', value: 'fourth'},
+          {key: 'five', value: 'fifth'},
+        ];
+
+        records.forEach(({key, value}) => {
+          bst.insert(key, value);
+        });
+
+        records.forEach(({key, value}) => {
+          expect(bst.lookup(key)).toBe(value);
+        });
+
+        records.reverse().forEach(({key, value}) => {
+          expect(bst.lookup(key)).toBe(value);
+        });
+      });
+    });
+
+    describe('insert', () => {
+      it('increases count by 1', () => {
+        expect(bst.count()).toBe(0);
+        bst.insert('test');
+        expect(bst.count()).toBe(1);
+
+        const keys = ['many', 'keys', 'for', 'this', 'tree'];
+        keys.forEach((key, i) => {
+          bst.insert(key);
+          expect(bst.count()).toBe(2 + i);
+        });
+      });
+
+      it('replaces records with the same key and does not increase the count', () => {
+        bst.insert('test', 'first value');
+        expect(bst.count()).toBe(1);
+        expect(bst.lookup('test')).toBe('first value');
+
+        bst.insert('test', 'second value');
+        expect(bst.count()).toBe(1);
+        expect(bst.lookup('test')).toBe('second value');
+      });
+
+      it('uses true as the default value', () => {
+        bst.insert('test');
+        expect(bst.lookup('test')).toBe(true);
+      });
     });
   });
 });
